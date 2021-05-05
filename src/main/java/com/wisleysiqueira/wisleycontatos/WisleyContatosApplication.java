6d26c934b1495d1ad732589453be3fef9ed1f2d4
wisleysiqueira <wisleysiqueira@hotmail.com>
2021-05-05 06:35:39 -0300

    Primeiro commit

---
+++ src/main/java/com/wisleysiqueira/wisleycontatos/WisleyContatosApplication.java
@@ -0,0 +1,13 @@
+package com.wisleysiqueira.wisleycontatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WisleyContatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WisleyContatosApplication.class, args);
+	}
+
+}
