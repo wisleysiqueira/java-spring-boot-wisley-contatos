4398dbd57ee141225646cea50b56eccc14bddc0e
wisleysiqueira <wisleysiqueira@hotmail.com>
2021-05-06 08:17:51 -0300

    Implementando cadastro de contatos

--- src/main/java/com/wisleysiqueira/wisleycontatos/ContatosControle.java
+++ src/main/java/com/wisleysiqueira/wisleycontatos/ContatosControle.java
@@ -1,9 +1,11 @@
 package com.wisleysiqueira.wisleycontatos;
 
 import java.util.ArrayList;
+import java.util.UUID;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -40,5 +42,14 @@ public class ContatosControle {
 		modelAndView.addObject("contato", new Contato());
 		
 		return modelAndView;
+	}
+	
+	@PostMapping("/contatos")
+	public String cadastrar(Contato contato) {
+		
+		String id = UUID.randomUUID().toString();
+		contato.setId(id);
+		LISTA_CONTATOS.add(contato);
+		return "redirect:/contatos";
 	}
 }
