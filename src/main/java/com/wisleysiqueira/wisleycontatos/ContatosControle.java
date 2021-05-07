8a595dd294587a67a29af569c4f2cdbed5cd56d7
wisleysiqueira <wisleysiqueira@hotmail.com>
2021-05-07 06:31:32 -0300

    Implementando Atualizar Contatos

--- src/main/java/com/wisleysiqueira/wisleycontatos/ContatosControle.java
+++ src/main/java/com/wisleysiqueira/wisleycontatos/ContatosControle.java
@@ -5,7 +5,9 @@ import java.util.UUID;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -51,5 +53,55 @@ public class ContatosControle {
 		contato.setId(id);
 		LISTA_CONTATOS.add(contato);
 		return "redirect:/contatos";
+	}
+	
+	@GetMapping("contatos/{id}/editar")
+	public ModelAndView editar(@PathVariable String id) {
+		ModelAndView modelAndView = new ModelAndView("formulario");
+		
+		Contato contato = procurarContato(id);
+		
+		modelAndView.addObject("contato", contato);
+		
+		return modelAndView;
+	}
+	
+	@PutMapping("/contatos/{id}")
+	public String atualizar(Contato contato) {
+		Integer indice = procurarIndiceContato(contato.getId());
+		
+		Contato contatoVelho = LISTA_CONTATOS.get(indice);
+		
+		LISTA_CONTATOS.remove(contatoVelho);
+		
+		LISTA_CONTATOS.add(indice, contato);
+		
+		return "redirect:/contatos";
+	}
+	
+	//Métodos auxiliares
+	
+	private Contato procurarContato(String id) {
+		Integer indice = procurarIndiceContato(id);
+		
+		if(indice != null) {
+			Contato contato = LISTA_CONTATOS.get(indice);
+			
+			return contato;
+		}
+		
+		return null;
+	}
+	
+	private Integer procurarIndiceContato(String id) {
+		for(int i = 0; i < LISTA_CONTATOS.size(); i++) {
+			Contato contato = LISTA_CONTATOS.get(i);
+			
+			if(contato.getId().equals(id)) {
+				return i;
+			}
+		}
+		
+		return null;
 	}
 }
